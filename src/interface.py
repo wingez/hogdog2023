6daddc793c8a5c593aca8216c59c1998211e23e9
AbbeClub <albin.j.k@hotmail.se>
2022-03-20 22:52:40 +0100

    bas

--- src/interface.py
+++ src/interface.py
@@ -1,4 +1,21 @@
+
 class Interface:
     #TODO
+    GPIO.add_event_detect(XX, GPIO.RISING, set_meat , bouncetime=300)
+    GPIO.add_event_detect(XX, GPIO.FALLING, reset_meat , bouncetime=300)
+
+    GPIO.add_event_detect(XX, GPIO.RISING, set_d1 , bouncetime=300)
+    GPIO.add_event_detect(XX, GPIO.FALLING, reset_d1 , bouncetime=300)
+
+    GPIO.add_event_detect(XX, GPIO.RISING, set_d2 , bouncetime=300)
+    GPIO.add_event_detect(XX, GPIO.FALLING, reset_d2 , bouncetime=300)
+
+    GPIO.add_event_detect(XX, GPIO.RISING,  , bouncetime=300)   
+    GPIO.add_event_detect(XX, GPIO.FALLING,  , bouncetime=300)
+
+    GPIO.add_event_detect(XX, GPIO.RISING,  , bouncetime=300)
+    GPIO.add_event_detect(XX, GPIO.FALLING,  , bouncetime=300)
+
+
     def placeholder():
         print("test")
