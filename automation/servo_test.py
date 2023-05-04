4b88027ebd6b9d587e3bceeeee4d7e361c6f0d95
Andreas <andreas.morosan@gmail.com>
2023-05-04 16:34:16 +0200

    lite jox

--- automation/servo_test.py
+++ automation/servo_test.py
@@ -3,10 +3,20 @@ import time
 
 #def servo_update_ang(ang, channel):
 #    kit.servo[channel].angle = ang
+kit = ServoKit(channels=16)
+kit.servo[0].actuation_range = 170 #Upper servo MAX
+kit.servo[0].actuation_range = 60 # Lower min?
+
 
 if __name__ == '__main__':
-    kit = ServoKit(channels=16)
-    kit.servo[0].angle = 0
-    kit.continuous_servo[4].throttle = 0.3
-    time.sleep(2)
-    kit.continuous_servo[4].throttle = 0
+    x = range(50, 160, 5)
+    for i in reversed(range(50,160,5)):
+        kit.servo[0].angle = int(i)
+        time.sleep(0.1)
+
+    #kit.servo[4].angle = 60
+    #kit.servo[0].angle = 50
+
+    #kit.continuous_servo[4].throttle = 0.3
+    #time.sleep(2)
+    #kit.continuous_servo[4].throttle = 0
