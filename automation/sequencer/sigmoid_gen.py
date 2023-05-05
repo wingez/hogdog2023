41986ecd1e21d9a22399459e5a0c13a99131bf91
Andreas <andreas.morosan@gmail.com>
2023-05-05 10:51:54 +0200

    added sigmoid generator+tester

---
+++ automation/sequencer/sigmoid_gen.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def servo_smoothed(start, end, step):
+  #LINSPACE MUST BE FROM -1.5 to 1.5 due to rounding problems (Higher eg. -2,2 is fine aswell)
+  y_values_int = [ round((end-start)*sigmoid(x)+start) for x in np.linspace(-1.5, 1.5, step)]
+  return y_values_int
+
+
+def sigmoid(x, k=4):
+    return (1 / (1 + np.exp(-k *x)))
+
+
+if __name__ == '__main__':
+  start = 50
+  end = 160
+  steps = 100
+  y_servo = servo_smoothed(50, 160, steps)
+  #print(y_servo)
+
---
+++ automation/sequencer/test_sigmoid.py
@@ -0,0 +1,36 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def servo_smoothed(start, end, step):
+  #LINSPACE MUST BE FROM -1.5 to 1.5 due to rounding problems (Higher eg. -2,2 is fine aswell)
+  y_values_int = [ round((end-start)*sigmoid(x)+start) for x in np.linspace(-1.5, 1.5, step)]
+  return y_values_int
+
+
+def sigmoid(x, k=4):
+    return (1 / (1 + np.exp(-k *x)))
+
+
+if __name__ == '__main__':
+  steps = 100
+  xy_values = [(x, sigmoid(x)) for x in np.linspace(0, 1, steps)]
+  x_values = [xy[0] for xy in xy_values]
+  y_servo_int = servo_smoothed(30, 70, steps)
+  print(y_servo_int)
+
+
+  # Prints the x and y values in the format (x, y)
+  # Create a new figure and axis object
+  fig, ax = plt.subplots()
+
+  # Plot the (x, y) values as a scatter plot
+  ax.scatter(x_values, y_servo)
+
+  # Set the axis labels
+  ax.set_xlabel('X')
+  ax.set_ylabel('Y')
+
+  # Show the plot
+  plt.show()
